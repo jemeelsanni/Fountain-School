2baeb4c5f5e33bed763986bdd0234de1a449562e
timmmyb <rotimib0@gmail.com>
2022-05-22 18:21:38 +0100

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      fountain: "#BB750C",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
