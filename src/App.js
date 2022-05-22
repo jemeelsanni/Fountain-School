489cc4db026a222694492f004cb2b9fdd2cb38f8
Bolu <boluadetifa@gmail.com>
2022-05-22 21:45:01 +0100

    nb

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="test" element={<Test />} />
-<<<<<<< HEAD
-        <Route path="omo" element={<Test/>} />
-=======
         <Route path="power" element={<Test />} />
->>>>>>> da19adcbf7c565c196fe392487f1ef7f797a0e6d
       </Routes>
     </div>
   );
