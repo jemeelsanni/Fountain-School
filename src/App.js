bcc9daf029691f3f0709be3d59b491e2da7f79bc
Bolu <boluadetifa@gmail.com>
2022-05-22 20:10:11 +0100

    omo

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="test" element={<Test />} />
+        <Route path="omo" element={<Test/>} />
       </Routes>
     </div>
   );
