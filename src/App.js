6319d7d81d6977a0d9cd26d4db1a4fa60953a721
Bolu <boluadetifa@gmail.com>
2022-05-23 12:37:56 +0100

    second nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TeacherSignin from "./auth/TeacherSignin";
 import Home from "./pages/Home";
 import Test from "./pages/Test";
+import SchoolFeePayment from "./pages/SchoolFeePayment";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="test" element={<Test />} />
         <Route path="power" element={<Test />} />
+        <Route path="teachersignin" element={<TeacherSignin />} />
+        <Route path="feepayment" element={<SchoolFeePayment/>} />
       </Routes>
     </div>
   );
