93fadc0c63be2313c1e937da558fc97110933d3f
shefihu <rotimib0@gmail.com>
2022-05-23 12:34:53 +0100

    userSignin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Test from "./pages/Test";
 import Omo from "./pages/Omo";
+import UserSignin from "./auth/UserSignin";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="test" element={<Test />} />
         <Route path="omo" element={<Omo />} />
+        <Route path="userauth" element={<UserSignin />} />
       </Routes>
     </div>
   );
