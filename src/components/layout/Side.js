18154057ea914635d566d42c3615b571f9b73798
shefihu <rotimib0@gmail.com>
2022-05-23 09:01:32 +0100

    update on folders

--- src/components/layout/Side.js
+++ src/components/layout/Side.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unknown-property */
 import React from "react";
-import Layout from "./Layout";
 
 const Dashboard = () => {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Layout from "../componets/Layout";
-import Side from "../componets/Side";
+import Layout from "../components/layout/Layout";
+import Side from "../components/layout/Side";
 
 const Home = () => {
   return (
-    <div className='px-4 lg:px-9'>
+    <div className="px-4 lg:px-9">
       <Layout />
       <div className="flex ">
         <Side />
