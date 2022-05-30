1740901cee47e4d0a27c69cc98fb724fd884fc32
Bolu <boluadetifa@gmail.com>
2022-05-30 13:30:48 +0100

    fix mobile nav

--- src/components/layout/web/navbar.js
+++ src/components/layout/web/navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className="flex justify-between px-20 py-6">
             <img src="./logo.jpg" className="w-14 h-12"/>
             <nav className="space-x-5">
-                <a className=" font-semibold hover:text-fountain" href="/">HOME</a>
+                <a className=" font-semibold hover:text-fountain" href="#">HOME</a>
                 <a className=" font-semibold hover:text-fountain" href="./About.js">ABOUT</a>
                 <a className=" font-semibold hover:text-fountain" href="./Contact.js">CONTACT US</a>
                 <a className=" font-semibold hover:text-fountain" href="">PORTAL</a>
